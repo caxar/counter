bc64ee71ebe798f8904ca3c1153e9b1e8e8524b4
vasya <ololohkalool@gmail.com>
2020-08-03 12:09:03 +0300

    add code into app.js

--- app.js
+++ app.js
@@ -1,64 +1,32 @@
 // 29f1c3
 // f03434
 
+const result = document.querySelector('.counter'),
+	  btns = document.querySelectorAll('.btn');
 
+let count = 0;
 
+btns.forEach(btn => {
+	btn.addEventListener('click', e => {
+		let currentTarget = e.currentTarget.classList;
+		if(currentTarget.contains('increase')) {
+			count++;
+		} else if(currentTarget.contains('decrease')) {
+			count--;
+		} else {
+			count = 0;
+		}
 
+		if(count > 0) {
+			result.style.color = '#29f1c3';
+		}
+		if(count < 0) {
+			result.style.color = '#f03434';
+		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const value = document.querySelector('.countNumber'),
-// 	  btns = document.querySelectorAll('.btn');
-
-// let count = 0;
-
-// btns.forEach(btn => {
-// 	btn.addEventListener('click', function(e) {
-// 		const currentIndex = e.currentTarget.classList;
-
-// 		if(currentIndex.contains('decrease')) {
-// 			count--;
-// 		} else if(currentIndex.contains('increase')) {
-// 			count++;
-// 		} else {
-// 			count = 0;
-// 		}
-
-// 		if(count < 0) {
-// 			value.style.color = '#f03434';
-// 		}
-// 		if(count > 0) {
-// 			value.style.color = '#29f1c3';
-// 		}
-// 		if(count === 0) {
-// 			value.style.color = '#fff';
-// 		}
-// 		value.innerText = count;
-// 	});
-// });
-
-
-
-
-
-
+		if(count === 0) {
+			result.style.color = '#fff';
+		}
+		result.innerText = count;
+	});
+});
