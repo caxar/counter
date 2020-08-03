870773f0d1dd9e2073786479ff253c9e8ecf9aec
vasya <ololohkalool@gmail.com>
2020-08-03 12:00:09 +0300

    add file style.css index.html and app.js

---
+++ app.js
@@ -0,0 +1,64 @@
+// 29f1c3
+// f03434
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const value = document.querySelector('.countNumber'),
+// 	  btns = document.querySelectorAll('.btn');
+
+// let count = 0;
+
+// btns.forEach(btn => {
+// 	btn.addEventListener('click', function(e) {
+// 		const currentIndex = e.currentTarget.classList;
+
+// 		if(currentIndex.contains('decrease')) {
+// 			count--;
+// 		} else if(currentIndex.contains('increase')) {
+// 			count++;
+// 		} else {
+// 			count = 0;
+// 		}
+
+// 		if(count < 0) {
+// 			value.style.color = '#f03434';
+// 		}
+// 		if(count > 0) {
+// 			value.style.color = '#29f1c3';
+// 		}
+// 		if(count === 0) {
+// 			value.style.color = '#fff';
+// 		}
+// 		value.innerText = count;
+// 	});
+// });
+
+
+
+
+
+
